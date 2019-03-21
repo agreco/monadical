a70af0ea582c235d5f537aa9496bbff97b86dd2f
Antonio Greco <agreco13@gmail.com>
2019-03-21 17:44:18 +0000

    Version bump 1.5.0 -> 1.5.1: Rebuild project

--- container.js
+++ container.js
@@ -7,31 +7,44 @@ exports.default = void 0;
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-var Container = function Container(val) {
-  var _this = this;
+var Container =
+/*#__PURE__*/
+function () {
+  function Container(val) {
+    var _this = this;
 
-  _classCallCheck(this, Container);
+    _classCallCheck(this, Container);
 
-  _defineProperty(this, "map", function (func) {
-    return func(_this._val);
-  });
+    _defineProperty(this, "map", function (func) {
+      return Container.of(func(_this._val));
+    });
 
-  _defineProperty(this, "fmap", function (func) {
-    return Container.of(func(_this._val));
-  });
+    _defineProperty(this, "join", function () {
+      return !_this._val instanceof Container ? _this : _this._val.join();
+    });
 
-  _defineProperty(this, "join", function () {
-    return !_this._val instanceof Container ? _this : _this._val.join();
-  });
+    _defineProperty(this, "toString", function () {
+      return "Container[".concat(_this._val, "]");
+    });
 
-  _defineProperty(this, "toString", function () {
-    return "Container[".concat(_this._val, "]");
-  });
+    this._val = val;
+  }
 
-  this._val = val;
-};
+  _createClass(Container, [{
+    key: "value",
+    get: function get() {
+      return this._val;
+    }
+  }]);
+
+  return Container;
+}();
 
 exports.default = Container;
 
