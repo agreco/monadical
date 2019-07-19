7ae62fdcc4da9796298b7d275a09fb2cc18b624b
Antonio Greco <agreco13@gmail.com>
2019-07-19 15:44:18 +0100

    [class-and-method-exposure]: Expose classes and methods from src via src/index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 
-import alt from '../alt';
-import compose from '../compose';
-import Container, { chainC, getOrElseC, mapC } from '../container';
-import curry from '../curry';
-import Either from '../either';
-import Empty from '../empty';
-import getPropOrElse from '../getPropOrElse';
-import identity from '../identity';
-import IO from '../io';
-import isDefined from '../isDefined';
-import isFunction from '../isFunction';
-import lift from '../lift';
-import Maybe from '../maybe';
-import noop from '../noop';
-import notNil from '../notNil';
-import notNull from '../notNull';
-import pipe from '../pipe';
-import seq from '../seq';
+import alt from './alt';
+import compose from './compose';
+import Container, { chainC, getOrElseC, mapC } from './container';
+import curry from './curry';
+import Either from './either';
+import Empty from './empty';
+import getPropOrElse from './getPropOrElse';
+import identity from './identity';
+import IO from './io';
+import isDefined from './isDefined';
+import isFunction from './isFunction';
+import lift from './lift';
+import Maybe from './maybe';
+import noop from './noop';
+import notNil from './notNil';
+import notNull from './notNull';
+import pipe from './pipe';
+import seq from './seq';
 import * as utils from './utils';
-import * as types from '../types';
+import * as types from './types';
 
 export {
   alt,
