25e8b7195f7d1bd8294518ef51d03d0f7c131012
Antonio Greco <20005+agreco@users.noreply.github.com>
2021-08-10 13:15:16 +0100

    fix-snake-casing: Correct snakeCase regex
    * Add regex to replace capital alpha chars with underscores throughout
      whole string - NOTE: Converts PascalCase/camelCase implicitly

--- src/snakeCase.ts
+++ src/snakeCase.ts
@@ -1,8 +1,9 @@
 const snakeCase: (val: string) => string = (val: string): string =>
   val
     .trim()
-    .toLowerCase()
-    .replace(/[^a-zA-Z0-9\p{L}]+/gu, '_')
-    .replace(/(\s_)/g, (match, p1) => p1.trimStart());
+    .replace(/[A-Z]/g, (match, offset) => `${offset > 0 ? '_' : ''}${match.toLowerCase()}`)
+    .replace(/[^a-z0-9\p{L}]+/gu, '_')
+    .replace(/(\s_)/g, (match, p1) => p1.trimStart())
+    .toLowerCase();
 
 export default snakeCase;
