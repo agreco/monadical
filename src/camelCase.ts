f3226c069d6f50cd503c2004e13873f1cacdf107
Antonio Greco <20005+agreco@users.noreply.github.com>
2021-09-06 15:33:26 +0100

    inline-regex-require

--- src/camelCase.ts
+++ src/camelCase.ts
@@ -1,11 +1,12 @@
-import { anyWhitespace, nonAlphaUnicode, oneOrMoreDigits } from './regex';
-
-const camelCase: (val: string) => string = (val: string): string =>
-  val
+const camelCase: (val: string) => string = (val: string): string => {
+  const { anyWhitespace, nonAlphaUnicode, oneOrMoreDigits } = require('./regex');
+  
+  return val
     .toLowerCase()
     .split(oneOrMoreDigits).join(' ')
     .replace(nonAlphaUnicode, ' ')
     .replace(anyWhitespace, (match, p1) => p1.toUpperCase().trimStart())
     .trim();
+};
 
 export default camelCase;
